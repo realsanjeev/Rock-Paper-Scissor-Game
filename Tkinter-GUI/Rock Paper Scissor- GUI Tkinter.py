c9252e6ed6fdaaf3d2ff898aa93130349298575d
realsanjeev <realsanjeev2@gmail.com>
2022-09-01 11:20:28 -0400

    Adding result

--- Tkinter-GUI/Rock Paper Scissor- GUI Tkinter.py
+++ Tkinter-GUI/Rock Paper Scissor- GUI Tkinter.py
@@ -9,6 +9,14 @@ root.geometry('555x345')
 root.title('Rock paper Scissor - GUI')
 headerFont = ("Arial bold", 30)
 
+def result():
+    text = '''Paper cover Rock.
+            Rock destroy Scissor.
+            Scissor cut paper'''
+    resultFont= ('Arial bold', 25)
+    resultLabel = Label(text=text, font=resultFont, fg='Gray')
+    resultLabel.pack(side='bottom')
+
 header = Label(text='Rock Paper Scissor', font=headerFont, bg='black', fg='White')
 header.pack(fill='x')
 
@@ -18,8 +26,11 @@ for path, _, files in os.walk(IMAGE_DIR_PATH):
         if file[-3:] == 'PNG':
             print(file)
             photo = PhotoImage(file=os.path.join(path, file))
+            print(photo)
             image_label = Label(image=photo)
             image_label.pack()
+result()
 
 if __name__=="__main__":
     root.mainloop()
+    
