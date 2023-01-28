3fa457dec0debc0dc2b8dd207f70de061a5164d0
realsanjeev <realsanjeev2@gmail.com>
2023-01-28 19:31:10 +0545

    Adding icon in window

--- Online Game/main.py
+++ Online Game/main.py
@@ -4,34 +4,37 @@ import pygame
 from connection import Network
 from button_handler import Button
 
+pygame.init()
 pygame.font.init()
-# create window
-WIDTH = 600
-HEIGHT = 600
 
+# WINDOW INFO
+WIDTH = 650
+HEIGHT = 600
 WIN_POS_Y = 0
 WIN_POS_Y = 20
-
+ICON = pygame.image.load('icon.png') 
 win = pygame.display.set_mode((WIDTH, HEIGHT), WIN_POS_Y, WIN_POS_Y)
+
 pygame.display.set_caption("Client")
+pygame.display.set_icon(ICON)
 
 def redraw_window(win, game, player):
     '''
-    Game window
+    Redraw Window GUI
     '''
     win.fill((128,128,128))
 
     if not game.connection():
-        font = pygame.font.SysFont('comicsans', 80)
+        font = pygame.font.SysFont('comicsans', 70)
         text = font.render('Waiting for player...!', 1, (255,0,0), True)
-        win.blit(text, (WIDTH/2 - text.get_width()/2, HEIGHT/2 - text.get_height()/2))
+        win.blit(text, (WIDTH - text.get_width(), HEIGHT/2 - text.get_height()/2))
     else:
         font = pygame.font.SysFont('comicsans', 50)
-        text = font.render('Your move', 1, (0,255,255))
-        win.blit(text, (80, 200))
+        text = font.render('Your Move', 1, (0,255,255))
+        win.blit(text, (50, 200))
 
         text = font.render('Opponent', 1, (0,255,255))
-        win.blit(text, (380, 200))
+        win.blit(text, (350, 200))
 
         move1 = game.get_player_move(0)
         move2 = game.get_player_move(1)
@@ -42,23 +45,23 @@ def redraw_window(win, game, player):
             if game.p1Action and player == 0:
                 text1 = font.render(move1, 1, (0,0,0))
             elif game.p1Action:
-                text1 = font.render('Locked In!!', 1, (0,0,0))
+                text1 = font.render('Locked In!', 1, (0,0,0))
             else:
                 text1 = font.render('Waiting....', 1, (0,0,0))
 
             if game.p2Action and player == 1:
                 text2 = font.render(move2, 1, (0,0,0))
             elif game.p2Action:
-                text2 = font.render('Locked in',1, (0,0,0))
+                text2 = font.render('Locked In!',1, (0,0,0))
             else:
                 text2 = font.render('Waiting...',1, (0,0,0))
 
         if player == 1:
-            win.blit(text2, (100,350))
-            win.blit(text1, (400,350))
+            win.blit(text2, (50,350))
+            win.blit(text1, (350,350))
         else:
-            win.blit(text1, (100,350))
-            win.blit(text2, (400,350))
+            win.blit(text1, (50,350))
+            win.blit(text2, (350,350))
 
         for btn in buttons:
             btn.draw(win)
@@ -79,16 +82,16 @@ def main():
     clock = pygame.time.Clock()
     network = Network()
     player = int(network.getid())
-    print ('You are a player', player)
+    pygame.display.set_caption(f"Player: {player}")
 
     while session:
         clock.tick(60)
         try:
             game = network.send('get')
-            print('='*20, game, '='*20)
+            # print('='*20, game, '='*20)
         except socket.error as err:
             session = False
-            print("couldn't get game", err)
+            # print("couldn't get game", err)
             break
 
         if game.action():
@@ -103,18 +106,18 @@ def main():
 
             font = pygame.font.SysFont('comicsans', 45)
             if (game.winner() == 1 and player == 1) or (game.winner() == 0 and player == 0):
-                text = font.render('You won!', 1, (255,0,128))
+                text = font.render('You won!!!', 1, (255,0,128))
             elif game.winner() == -1:
-                text = font.render('Tie Game!', 1, (255,0,0))
+                text = font.render('Tie Game!!!', 1, (255,0,0))
             else:
                 text = font.render('You Lost!', 1, (255,0,0))
 
             win.blit(text, (WIDTH/2 - text.get_width()/2, HEIGHT/2 - text.get_height()/2))
             pygame.display.update()
-            pygame.time.delay(2000)
+            pygame.time.delay(3000)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                session =False
+                session = False
                 pygame.quit()
 
             if event.type == pygame.MOUSEBUTTONDOWN:
@@ -145,11 +148,12 @@ def menu_screen():
         text = font.render(intro_text, 1, (255,0,0))
         win.blit(text, (WIDTH/2 - text.get_width()/2, HEIGHT/2 - text.get_height()/2))
         pygame.display.update()
-
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 session = False
+                exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 session = False
     main()
