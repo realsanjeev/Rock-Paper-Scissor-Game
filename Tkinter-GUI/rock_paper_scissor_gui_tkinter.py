5b55be109d0859bf6fd954d2c1eb2f586c014a31
realsanjeev <realsanjeev2@gmail.com>
2023-01-28 21:42:14 +0545

    Adding images in tkinker GUI

--- Tkinter-GUI/rock_paper_scissor_gui_tkinter.py
+++ Tkinter-GUI/rock_paper_scissor_gui_tkinter.py
@@ -1,37 +1,41 @@
 '''import library for GUI'''
-import os
-from tkinter import Tk, Label, PhotoImage
+import glob
+import tkinter as tk
+from PIL import Image, ImageTk   
 
-IMAGE_DIR_PATH = 'images'
+IMAGE_DIR_PATH = glob.glob('*/*.png')
+SIZE = (50, 50)
 
-root = Tk()
+root = tk.Tk()
 root.geometry('555x345')
 root.title('Rock paper Scissor - GUI')
 headerFont = ("Arial bold", 30)
+header = tk.Label(text='Rock Paper Scissor - Game', font=headerFont, bg='black', fg='White')
+header.pack(fill='x')
 
 def result():
     '''
     result for output
     '''
-    text = '''Paper cover Rock.
-            Rock destroy Scissor.
-            Scissor cut paper'''
+    text = '''Paper cover Rock.\nRock destroy Scissor.\nScissor cut paper'''
     result_font = ('Arial bold', 25)
-    result_label = Label(text=text, font=result_font, fg='Gray')
+    result_label = tk.Label(text=text, font=result_font, fg='Gray')
     result_label.pack(side='bottom')
 
-header = Label(text='Rock Paper Scissor', font=headerFont, bg='black', fg='White')
-header.pack(fill='x')
 
-for path, _, files in os.walk(IMAGE_DIR_PATH):
-    for file in files:
-        # print(file)
-        if file[-3:] == 'PNG':
-            print(file)
-            photo = PhotoImage(file=os.path.join(path, file))
-            print(photo)
-            image_label = Label(image=photo)
-            image_label.pack()
+print('*'*100)
+print(IMAGE_DIR_PATH)
+imgs = []
+for file in IMAGE_DIR_PATH:
+    if file[-3:]=='png':
+        im = Image.open(file)
+        im.thumbnail(SIZE)
+        imgs.append(ImageTk.PhotoImage(im))
+        
+        photo = tk.PhotoImage(file=file)
+        ilabel = tk.Label(root, image=imgs[-1])
+        ilabel.pack(side='left')
+
 result()
 
 if __name__=="__main__":
